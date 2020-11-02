cd3100289092cf2b47007bb8f24fb823b49db4ca
rizkytegar <mail.rizkytegar@gmail.com>
2020-11-02 09:02:48 +0700

    m

--- list-siswa.php
+++ list-siswa.php
@@ -14,6 +14,37 @@
     <nav>
     <a href="form-daftar.php">[ + ] Tambah Buka</a>
     </nav>
-    
+    <table border="1">
+    <thead>
+    <tr>
+    <th>No</th>
+    <tr>Nama</tr>
+    <tr>Alamat</tr>
+    <tr>Jenis Kelamin</tr>
+    <tr>Agama</tr>
+    <tr>Tindakan</tr>
+    </tr>
+    </thead>
+    <tbody>
+    <?php
+$sql = "SELECT * FROM calon_siswa";
+$query = mysqli_query($db, $sql);
+while($siswa = mysqli_fetch_array($query)){
+echo "<tr>";
+echo "<td>".$siswa['id']."</td>";
+echo "<td>".$siswa['nama']."</td>";
+echo "<td>".$siswa['alamat']."</td>";
+echo "<td>".$siswa['jenis_kelamin']."</td>";
+echo "<td>".$siswa['agama']."</td>";
+echo "<td>".$siswa['sekolah_asal']."</td>";
+echo "<td>";
+echo "<a href='form-edit.php?id=".$siswa['id']."'>Edit</a> | ";
+echo "<a href='hapus.php?id=".$siswa['id']."'>Hapus</a>";
+echo "</td>";
+echo"</tr>";
+}
+    ?>
+    </tbody>
+    </table>
 </body>
 </html>
